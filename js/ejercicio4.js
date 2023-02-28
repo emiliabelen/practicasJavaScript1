164369bc7e876a25cf9d48e5120a4613013c1302
belen ramos <ramos.emiliabelen@gmail.com>
2023-02-28 19:30:00 -0300

    listo ejercicio4

---
+++ js/ejercicio4.js
@@ -0,0 +1,6 @@
+//4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+
+
+const nombre = prompt ("Ingrese su nombre de usuario");
+// document.write("Hola" +" " + nombre); 
+document.write (`hola ${nombre} `)
