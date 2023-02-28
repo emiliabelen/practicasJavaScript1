d57fb398d071a0cf535a7dbc0f80d973f0fab630
belen ramos <ramos.emiliabelen@gmail.com>
2023-02-28 19:46:43 -0300

    listo ejercicio5

---
+++ js/ejercicio5.js
@@ -0,0 +1,7 @@
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+// Nota: Tener en cuenta la siguiente función: parseInt
+
+let numero1 = parseInt(prompt("Ingrese el primer numero"));
+let numero2 = parseInt(prompt("Ingrese el segundo numero"));
+document.write(numero1 + numero2);
+
