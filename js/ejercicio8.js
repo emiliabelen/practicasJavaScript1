f206f56052450486f4cf0f00921481a4b5be449c
belen ramos <ramos.emiliabelen@gmail.com>
2023-02-28 20:23:03 -0300

    listo ejercicio8

---
+++ js/ejercicio8.js
@@ -0,0 +1,9 @@
+//8.- Escribe un programa que pida un número y diga si es divisible por 2
+let numero = parseInt(prompt("Ingrese el numero"));
+if  (numero%2 === 0) {
+    document.write("el numero es divisible por 2");
+}
+
+    else { document.write("el numero no es divisible por 2");
+}
+
