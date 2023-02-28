dcff16874bdd0626da0a25100c29cf9913bb5457
belen ramos <ramos.emiliabelen@gmail.com>
2023-02-28 20:38:19 -0300

    listo ejercicio9

---
+++ js/ejercicio9.js
@@ -0,0 +1,28 @@
+//9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+// Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+
+const frase = prompt("ingrese una frase");
+frase.toLowerCase();
+let vocales = "";
+for (let i = 0; i < frase.length; i++) {
+  const element = frase[i];
+  switch (element) {
+    case "a":
+      vocales += frase.charAt(i) + " ";
+      break;
+    case "e":
+      vocales += frase.charAt(i) + " ";
+      break;
+    case "i":
+      vocales += frase.charAt(i) + " ";
+      break;
+    case "o":
+      vocales += frase.charAt(i) + " ";
+      break;
+    case "u":
+      vocales += frase.charAt(i) + " ";
+      break;
+  }
+}
+
+document.write (vocales)
